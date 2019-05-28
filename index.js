b03f80118869128d86018f8f019be9524acacb7c
unknown <fehshuffle@gmail.com>
2019-05-28 20:09:45 -0300

    some changes

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.post('/', (req, res) => {
+  let data = req.body;
+  let year = Object.keys(data)
+  let values = Object.values(data)
+  console.log(year,values)
+ 
+});
+
 app.listen(3000, () => {
-  console.log('Running local host' + 3000);
+  console.log('Running local host ' + 3000);
 });
